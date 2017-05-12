884ad97ddbdb484f3d54d8ee278bec682476cbc9
prenjis <prenjis@PRENJISPC>
2017-05-12 19:05:01 +0300

    Fixed an issue with the db connection string in config Added a controller for error pages added 2 views for error pages added extensions added member repository recreated repositories project added dbcontext

---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking.Web/Extensions/IdentityExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace SmartOnStreetParking.Web.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetProperty(this IIdentity identity, string PropertyName)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst(PropertyName);
+            // Test for null to avoid issues during local testing
+            return (claim != null) ? claim.Value : "0";
+        }
+    }
+
+}
---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking.Web/Utils/BaseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartOnStreetParking.Web.Utils
+{
+    public class BaseController: Controller
+    {
+
+        /// <summary>
+        /// Default Error handling for controller.
+        /// </summary>
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+            {
+                string controller = filterContext.RouteData.Values["controller"].ToString();
+                string action = filterContext.RouteData.Values["action"].ToString();
+
+                Exception ex = filterContext.Exception;
+                //do something with these details here
+
+                filterContext.ExceptionHandled = true;
+                //if (ex is System.Web.Http.HttpResponseException)
+                //{
+                //    if ((ex as System.Web.Http.HttpResponseException).Response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                //    {
+                //        filterContext.Result = RedirectToAction("NoDataError", "Pages");
+                //    }
+                //    else if ((ex as System.Web.Http.HttpResponseException).Response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                //    {
+                //        filterContext.Result = RedirectToAction("BadRequestError", "Pages");
+                //    }
+                //}
+                //else
+                //{
+                //    //log.Error(ex.Message, ex);
+                    TempData["CustomError"] = ex.Message;
+                    filterContext.Result = RedirectToAction("InternalServerError", "Pages");
+                //}
+
+                base.OnException(filterContext);
+            }
+        }
+    }
+}
