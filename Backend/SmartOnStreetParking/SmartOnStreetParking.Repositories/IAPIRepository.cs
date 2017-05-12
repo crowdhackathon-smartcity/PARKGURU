d98815923d9bd35c3d49a4ab5886cf474481da81
Ioannis Zarifis <jzarifis@gmail.com>
2017-05-12 23:48:06 +0300

    Push repositories

---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking.Repositories/IAPIRepository.cs
@@ -0,0 +1,20 @@
+﻿using SmartOnStreetParking.Models;
+using SmartOnStreetParking.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartOnStreetParking.Repositories
+{
+    public interface IAPIRepository
+    {
+        List<ParkingSpotResponse> SearchSpots(SearchSpotsRequest SearchSpotsRequest);
+
+        SpotTicketsResponse CalcSpotTickets(CalcTicketsRequest CalcTicketsRequest);
+
+        bool UpdateDummyRecordWithSpatial(long SpotId, List<Coordinate> Edges);
+
+    }
+}
