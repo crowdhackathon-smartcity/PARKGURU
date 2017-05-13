f4dd1cb3753a2586b77a82362bdd6569bd911ef5
prenjis <prenjis@PRENJISPC>
2017-05-13 13:23:13 +0300

    add zone form

--- Backend/SmartOnStreetParking/SmartOnStreetParking/Controllers/ZoneController.cs
+++ Backend/SmartOnStreetParking/SmartOnStreetParking/Controllers/ZoneController.cs
@@ -33,7 +33,43 @@ namespace SmartOnStreetParking.Web.Controllers
             return View(RetVal);
         }
 
+        public ActionResult Add()
+        {
+            AddZoneViewModel ViewModel = new AddZoneViewModel();
 
+            ViewModel.MemberId = GetMember().Id;
+
+
+            return View(ViewModel);
+        }
+
+        /// <summary>
+        /// Add a new bank account to the database.
+        /// </summary>
+        /// <param name="AccountInfo">The details of the new account.</param>
+        /// <returns>ActionResult</returns>
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Add(AccountSettings_ViewModel AccountInfo)
+        //{
+        //    if (!ModelState.IsValid) { return View(AccountInfo); }
+
+        //    var config = new MapperConfiguration(cfg => cfg.CreateMap<AccountSettings_ViewModel, AccountSettings>());
+        //    var mapper = config.CreateMapper();
+        //    AccountSettings NewAccount = mapper.Map<AccountSettings_ViewModel, AccountSettings>(AccountInfo);
+
+        //    _Repository.Add(NewAccount);
+
+        //    if (AccountInfo.Type == BankingAccountType.Bank)
+        //    {
+
+        //        return RedirectToAction(BankingControllerIndex.BankIndex);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction(BankingControllerIndex.PaypalIndex);
+        //    }
+        //}
 
     }
 
---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking/Models/AddZoneViewModel.cs
@@ -0,0 +1,41 @@
+﻿using SmartOnStreetParking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartOnStreetParking.Web.Models
+{
+    public class AddZoneViewModel: Zone
+    {
+
+        
+
+        public TimeItemViewModel D1 { get; set; }
+        public TimeItemViewModel D2 { get; set; }
+        public TimeItemViewModel D3 { get; set; }
+        public TimeItemViewModel D4 { get; set; }
+        public TimeItemViewModel D5 { get; set; }
+        public TimeItemViewModel D6 { get; set; }
+        public TimeItemViewModel D7 { get; set; }
+
+
+        public AddZoneViewModel()
+        {
+            D1 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Sunday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D2 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Monday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D3 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Tuesday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D4 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Wednesday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D5 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Thursday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D6 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Friday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+            D7 = new TimeItemViewModel() { Checked = true, Days = DayOfWeek.Saturday, Start = new TimeSpan(0, 0, 0, 0), End = new TimeSpan(1, 0, 0, 0) };
+        }
+    }
+
+    public class TimeItemViewModel: ParkingTimeTable
+    {
+        public bool Checked { get; set; }
+        
+
+    }
+}
