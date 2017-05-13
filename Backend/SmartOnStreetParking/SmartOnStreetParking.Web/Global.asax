ba610bc22bc24ad4a64c02ed5cdcc6b853ff6a35
prenjis <prenjis@PRENJISPC>
2017-05-13 12:02:37 +0300

    Zone index

--- Backend/SmartOnStreetParking/SmartOnStreetParking.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartOnStreetParking.Web.MvcApplication" Language="C#" %>
---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking/Controllers/ZoneController.cs
@@ -0,0 +1,42 @@
+﻿using SmartOnStreetParking.Models;
+using SmartOnStreetParking.Repositories;
+using SmartOnStreetParking.Web.Models;
+using SmartOnStreetParking.Web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace SmartOnStreetParking.Web.Controllers
+{
+    public class ZoneController : BaseController
+    {
+        private readonly IZoneRepository _Repository;
+
+        public ZoneController()
+        {
+            _Repository = new ZoneRepository();
+        }
+
+        public ZoneController(IZoneRepository Repository)
+        {
+            _Repository = Repository;
+        }
+
+        public ActionResult Index()
+        {
+
+            List<Zone> RetVal = _Repository.GetAll();
+            
+            return View(RetVal);
+        }
+
+
+
+    }
+
+    
+
+}
