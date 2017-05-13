58b7f33acaa89936fbe8b78942c6b274a47bc98e
prenjis <prenjis@PRENJISPC>
2017-05-13 18:43:56 +0300

    Add Spot!!!

---
+++ Backend/SmartOnStreetParking/Repositories/ParkingSpotRepository.cs
@@ -0,0 +1,46 @@
+﻿using SmartOnStreetParking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartOnStreetParking.Repositories
+{
+    public class ParkingSpotRepository: IParkingSpotRepository
+    {
+
+        public List<ParkingSpot> GetByMember(Int64 MemberId)
+        {
+
+            using (var DBContext = new SmartOnStreetParkingDbContext())
+            {
+                DBContext.Configuration.ProxyCreationEnabled = false;
+                var ParkingSpots = DBContext.ParkingSpots.Where(ps => ps.Zone.MemberId == MemberId);
+                if (ParkingSpots.Count() == 0)
+                {
+                    return new List<ParkingSpot>();
+                }
+                else
+                {
+                    return DBContext.ParkingSpots.ToList();
+                }
+            }
+        }
+
+
+        public ParkingSpot Add(ParkingSpot SpotInfo, List<Coordinate> Edges)
+        {
+            SpotInfo.GeometryEdges = Edges;
+
+            using (var DBContext = new SmartOnStreetParkingDbContext())
+            {
+                DBContext.ParkingSpots.Add(SpotInfo);
+                DBContext.SaveChanges();
+            }
+            return SpotInfo;
+        }
+
+
+    }
+}
---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking/Models/AddParkingSpotViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SmartOnStreetParking.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace SmartOnStreetParking.Web.Models
+{
+    public class AddParkingSpotViewModel : ParkingSpot
+    {
+        public string StringEdges { get; set; }
+
+
+
+
+        public string EdgesToJson()
+        {
+            var coordString = "";
+            if (GeometryEdges != null && GeometryEdges.Count > 0)
+            {
+                foreach (var item in GeometryEdges)
+                {
+                    coordString = coordString + string.Format("{{\"lng\":{0} ,\"lat\": {1}}},", item.Longitude.ToString(CultureInfo.InvariantCulture), item.Latitude.ToString(CultureInfo.InvariantCulture));
+                }
+                coordString = "[" + coordString.Substring(0, coordString.Length - 1) + "]";
+            }
+            return coordString;
+        }
+    }
+}
