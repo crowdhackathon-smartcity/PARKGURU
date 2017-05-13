dcbc1a2025c5d175d9e24fb1b563ed733e6a788b
Evgenios Kontaxakis <ekontaxakis@gmail.com>
2017-05-13 15:47:29 +0300

    Payments

---
+++ Backend/SmartOnStreetParking/Repositories/PaymentRepository.cs
@@ -0,0 +1,36 @@
+﻿using SmartOnStreetParking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartOnStreetParking.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        /// Class Constructor
+        /// </summary>
+        public PaymentRepository()
+        {
+
+        }
+
+        /// <summary>
+        /// Get all Payments
+        /// </summary>
+        public List<Payment> GetAll()
+        {
+
+            using (var DBContext = new SmartOnStreetParkingDbContext())
+            {
+                DBContext.Configuration.ProxyCreationEnabled = false;
+                var zones = DBContext.Zones;
+                if (zones.Count() == 0)
+                    return new List<Payment>();
+                else
+                    return DBContext.Payments.ToList();
+            }
+        }
+    }
+}
---
+++ Backend/SmartOnStreetParking/SmartOnStreetParking/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using SmartOnStreetParking.Models;
+using SmartOnStreetParking.Repositories;
+using SmartOnStreetParking.Web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartOnStreetParking.Web.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        private readonly IPaymentRepository _Repository;
+
+        public PaymentController()
+        {
+            _Repository = new PaymentRepository();
+        }
+
+        public PaymentController(IPaymentRepository Repository)
+        {
+            _Repository = Repository;
+        }
+
+        public ActionResult View()
+        {
+
+            List<Payment> RetVal = _Repository.GetAll();
+
+            return View(RetVal);
+        }
+    }
+}
